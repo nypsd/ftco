d8c4d4da782a36c98b0cac23701d4a4d9562b027
Becky Stewart <becky@theleadingzero.com>
2018-04-16 19:12:50 +0100

    Version used in first draft of content

--- session_2/step1_on-off-LED/step1_on-off-LED.ino
+++ session_2/step1_on-off-LED/step1_on-off-LED.ino
@@ -1,42 +1,25 @@
 /* On-Off LED Sketch
-*  Circuit: resistor connected to Pin 7
-*           LED long leg connected to resistor
-*           LED short leg connected to GND
+*   LED connected on Digital Pin 7
 */
 
-// setup() is the label for the code between the { } 
-// every Arduino sketch needs one (and only one)
-// section with this label.
-// The code in setup() will be run once right when the 
-// Arduino turns on or starts running its code.
-
-// 'void' in front of setup() and loop() means that once
-// all the code has been run, there is nothing else
-// that needs to be done.
-
+// the code in setup() is run once
 void setup() { 
-  // Need to say that the pin will send out current
-  // as opposed to measuring the current coming in.
-  // '7' is the pin we are talking about
-  // note that 'OUTPUT' is all upper case letters
+  // Pin 7 will send out electricity
   pinMode(7, OUTPUT);
 
 }
 
-// loop() is the label for the code between { }
-// every Arduino sketch needs one (and only one)
-// section with this label.
-// The code in loop() will first be run after setup()
-// finishes and then run repeatedly until the Arduino
-// is turned off
+
+// the code in loop() will first be run after setup()
+// finishes and then run repeatedly
 void loop() {
   // turn on the power at Pin 7
   digitalWrite(7, HIGH);
 
-  // wait for 1000 ms (1 second)
-  delay(1000);
+  // wait for 2000 ms (2 seconds)
+  delay(2000);
 
-  // turn off the power a Pin 7
+  // turn off the power at Pin 7
   digitalWrite(7, LOW);
 
   // wait for 1000 ms (1 second)
